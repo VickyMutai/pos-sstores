8a3c49c58e5673dc5ab1d614ee5618b845e4417f
VickyMutai <vicky.mutai96@gmail.com>
2020-10-04 00:11:10 +0300

    Setting up profile

---
+++ order.php
@@ -0,0 +1,111 @@
+<?php
+    include_once'db/connect_db.php';
+    session_start();
+    if($_SESSION['username']==""){
+        header('location:index.php');
+    }else{
+        if($_SESSION['role']=="Admin"){
+          include_once'inc/header_all.php';
+        }else{
+            include_once'inc/header_all_operator.php';
+        }
+    }
+
+    error_reporting(0);
+
+    $id = $_GET['id'];
+
+    $delete_query = "DELETE invoice , invoice_detail FROM invoice INNER JOIN invoice_detail ON invoice.invoice_id =
+    invoice_detail.invoice_id WHERE invoice.invoice_id=$id";
+    $delete = $pdo->prepare($delete_query);
+    if($delete->execute()){
+        echo'<script type="text/javascript">
+            jQuery(function validation(){
+            swal("Info", "Transaction has been deleted", "info", {
+            button: "Continue",
+                });
+            });
+            </script>';
+    }
+?>
+
+<html>
+<head>
+<meta http-equiv="refresh" content="60">
+</head>
+</html>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Transactions
+      </h1>
+      <hr>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+        <div class="box box-success">
+            <div class="box-header with-border">
+                <h3 class="box-title">Transaction List</h3>
+                <a href="create_order.php" class="btn btn-success btn-sm pull-right">Add Transaction</a>
+            </div>
+            <div class="box-body">
+                <div style="overflow-x:auto;">
+                    <table class="table table-striped" id="myOrder">
+                        <thead>
+                            <tr>
+                                <th style="width:20px;">No</th>
+                                <th style="width:100px;">Cashier Name</th>
+                                <th style="width:100px;">Date</th>
+                                <th style="width:100px;">Price</th>
+                                <th style="width:50px;">Option</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            $select = $pdo->prepare("SELECT * FROM invoice ORDER BY invoice_id DESC");
+                            $select->execute();
+                            while($row=$select->fetch(PDO::FETCH_OBJ)){
+                            ?>
+                                <tr>
+                                <td><?php echo $no++ ; ?></td>
+                                <td class="text-uppercase"><?php echo $row->cashier_name; ?></td>
+                                <td><?php echo $row->order_date; ?></td>
+                                <td>Rp. <?php echo number_format($row->total); ?></td>
+                                <td>
+                                    <?php if($_SESSION['role']=="Admin"){ ?>
+                                    <a href="order.php?id=<?php echo $row->invoice_id; ?>" onclick="return confirm('Delete Transaction?')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                                    <?php } ?>
+                                    <a href="misc/nota.php?id=<?php echo $row->invoice_id; ?>" target="_blank" class="btn btn-info btn-sm"><i class="fa fa-print"></i></a>
+                                </td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <script>
+  $(document).ready( function () {
+      $('#myOrder').DataTable();
+  } );
+  </script>
+
+ <?php
+    include_once'inc/footer_all.php';
+ ?>
