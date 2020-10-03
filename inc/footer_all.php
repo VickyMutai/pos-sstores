757eeae0865694e8a4d622481dad93f5f0f65708
VickyMutai <vicky.mutai96@gmail.com>
2020-10-03 22:23:12 +0300

    Setting up categories and products

---
+++ inc/footer_all.php
@@ -0,0 +1,22 @@
+ <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Sambai Stores |POS Ver. 1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Developed by </strong> <a href="https://vickymutai.github.io/" target="_blank">Legacy Softwares</a>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- jQuery 3 -->
+<script src="js/jquery.js"></script>
+<!-- DataTables -->
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+
+</body>
+</html>
